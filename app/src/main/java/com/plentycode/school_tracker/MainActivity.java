8f30c45fbbbcc720cc913feb3c6dcc3561606450
Iván <c_ivanmercado@hotmail.com>
2016-08-16 23:17:15 -0500

    added google login

--- app/src/main/java/com/plentycode/school_tracker/MainActivity.java
+++ app/src/main/java/com/plentycode/school_tracker/MainActivity.java
@@ -1,13 +1,36 @@
 package com.plentycode.school_tracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+
+    private String loggedName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase auth
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        if (firebaseUser == null) {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return;
+        } else {
+            loggedName = firebaseUser.getDisplayName();
+        }
+
     }
 }
