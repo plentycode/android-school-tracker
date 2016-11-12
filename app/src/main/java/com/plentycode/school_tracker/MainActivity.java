f6f313ea5a39acb855b36a91f7c450b25f148949
Iván <c_ivanmercado@hotmail.com>
2016-11-12 14:43:31 -0500

    Added Firebase notification messages  - updated firebase version pks  - cleanned up some unuse code

--- app/src/main/java/com/plentycode/school_tracker/MainActivity.java
+++ app/src/main/java/com/plentycode/school_tracker/MainActivity.java
@@ -7,9 +7,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
@@ -40,13 +44,41 @@ public class MainActivity extends AppCompatActivity {
         } else {
             loggedName = firebaseUser.getDisplayName();
             Log.d(TAG, "User Logged" + loggedName);
+            Toast.makeText(MainActivity.this, "Welcome, " + loggedName, Toast.LENGTH_SHORT).show();
 
             TextView txtCentered = (TextView) findViewById(R.id.centered_text);
             txtCentered.setText("Welcome, " + loggedName);
         }
 
-        btnLogOut = (Button) findViewById(R.id.log_out);
 
+        // subscribe button
+        Button btnSubscribe = (Button) findViewById(R.id.btnSubscribe);
+        btnSubscribe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseMessaging.getInstance().subscribeToTopic("news");
+
+                // Log and toast
+                Toast.makeText(MainActivity.this, "Subscribed to News", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button logTokenButton = (Button) findViewById(R.id.btnShowToken);
+        logTokenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get token
+                String token = FirebaseInstanceId.getInstance().getToken();
+
+                // Log and toast
+                String msg = getString(R.string.msg_token_fmt, token);
+                Log.d(TAG, msg);
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // log out button
+        btnLogOut = (Button) findViewById(R.id.log_out);
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,6 +86,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
+
+
+        /* MESSAGING */
+
+        // If a notification message is tapped, any data accompanying the notification
+        // message is available in the intent extras. In this sample the launcher
+        // intent is fired when the notification is tapped, so any accompanying data would
+        // be handled here. If you want a different intent fired, set the click_action
+        // field of the notification message to the desired intent. The launcher intent
+        // is used when no click_action is specified.
+        //
+        // Handle possible data accompanying notification message.
+        if (getIntent().getExtras() != null) {
+            for (String key : getIntent().getExtras().keySet()) {
+                Object value = getIntent().getExtras().get(key);
+                Log.d(TAG, "Key: " + key + " Value: " + value);
+            }
+        }
 
     }
 }
